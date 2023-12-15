3fe2d6f0ff555c542df695e88365af6e2293ff4e
Vincenzo Padula <padvincenzo@gmail.com>
2023-12-15 16:31:16 +0100

    Fix version compatibility

--- plugin.php
+++ plugin.php
@@ -12,8 +12,8 @@
 $id = "pcnlj";
 $version = "1.0";
 
-$ilias_min_version = "8.0";
-$ilias_max_version = "8.999";
+$ilias_min_version = "6.0";
+$ilias_max_version = "7.999";
 
 $responsible = "OC Open Consulting";
 $responsible_mail = "plugins@oc-group.eu";
